1e74adf5abc05759e93b7092f24b76a4e64195bd
Nilson Escobar <escnil994@icloud.com>
2025-11-07 16:30:20 -0600

    Environment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-fastapii-deploy--0000003.wittyocean-829c2314.westus2.azurecontainerapps.io'
+  apiUrl: 'https://mi-portafolio-api.icyfield-c2e0f330.westus2.azurecontainerapps.io'
 };
