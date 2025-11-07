578374058334ff3df4c2ad73b81d8ed1713965d7
Nilson Escobar <escnil994@icloud.com>
2025-11-07 16:40:01 -0600

    Environment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mi-portafolio-api.icyfield-c2e0f330.westus2.azurecontainerapps.io'
+  apiUrl: 'https://mi-portafolio-api.icyfield-c2e0f330.westus2.azurecontainerapps.io/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://mi-portafolio-api.icyfield-c2e0f330.westus2.azurecontainerapps.io'
+  apiUrl: 'https://mi-portafolio-api.icyfield-c2e0f330.westus2.azurecontainerapps.io/api/v1'
 };
